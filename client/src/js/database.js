92f47e2419b4e2f380bc44e6ce083e4421462533
sheetaljwl795 <sheetaljwl795@gmail.com>
2022-11-14 14:25:34 -0800

    Fixed the debug issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,14 @@ const initdb = async () =>
     },
   });
 
+  
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>  {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
-  const request = store.put({ id: id, value: content });
+  const store = tx.objectStore('jate')
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value); 
 };
